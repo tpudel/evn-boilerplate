9f1c18540ca06b995749125068ca5ba2c585b625
Tommy P <thomaspud@gmail.com>
2017-10-22 02:42:40 -0400

    Finished winston logger, routes, and frontend basics

--- app/helpers/logger.js
+++ app/helpers/logger.js
@@ -1,30 +1,59 @@
 const winston = require('winston');
 const colors = require('colors/safe');
 
-const infoFormat = winston.format.printf(info => {
-    console.log(info)
-    const color = {
-        info: 'green',
-        warn: 'yellow',
-        error: 'red'
-    }[info.level] || 'white';
+/**
+ * Custom formatted winston logger.
+ * @param {string} label - nickname of the file
+ * @return {function} logger - the winston logger instance
+ */
 
-    const timestamp = new Date().toLocaleTimeString('en-us', {
-        day: '2-digit',
-        month: '2-digit',
-        year: '2-digit',
-        second: '2-digit',
-        minute: '2-digit',
-        hour: '2-digit'
-    }).split(',').join(' ');
+function init(label) {
 
-    return `[${colors.grey(timestamp)}] ${label} ${colors[color](info.level.toUpperCase())}: ${info.message}`;
-});
+    // Special colorized formatting for console output
+    const consoleFormat = winston.format.printf(function(info) {
+        // sets color based on 'info.level'. If level is undocumented, uses white.
+        const color = {
+            info: 'green',
+            warn: 'yellow',
+            error: 'red'
+        }[info.level] || 'white';
 
-const logger = winston.createLogger({
-    transports: [
-        new winston.transports.Console({ format: infoFormat })
-    ]
-});
+        // makes formatted timestamp, MM/DD/YY HH:MM:SS AM/PM
+        const timestamp = new Date().toLocaleTimeString('en-us', {
+            day: '2-digit',
+            month: '2-digit',
+            year: '2-digit',
+            second: '2-digit',
+            minute: '2-digit',
+            hour: '2-digit'
+        }).split(',').join(' ');
 
-module.exports = logger;
+        return `[${colors.grey(timestamp)}] (${colors.grey(label)}) ${colors[color](info.level.toUpperCase())}: ${info.message}`;
+    });
+
+    // Non-colored output for error.log
+    const errorFormat = winston.format.printf(function(info) {
+        const timestamp = new Date().toLocaleTimeString('en-us', {
+            day: '2-digit',
+            month: '2-digit',
+            year: '2-digit',
+            second: '2-digit',
+            minute: '2-digit',
+            hour: '2-digit'
+        }).split(',').join(' ');
+
+        return `[${timestamp}] (${label}) ${info.level.toUpperCase()}: ${info.message}`;
+    });
+
+    // Logger instance
+    const logger = winston.createLogger({
+        transports: [
+            new winston.transports.Console({ format: consoleFormat }),
+            new winston.transports.File({ format: errorFormat, filename: 'app/logs/error.log', level: 'error' })
+        ]
+    });
+    
+    return logger;
+}
+
+module.exports = init;
--- app/server.js
+++ app/server.js
@@ -1,10 +1,16 @@
-const logger = require('./helpers/logger.js');
+const logger = require('./helpers/logger.js')('server.js');
 
 const express = require('express');
 const app = express();
 
 const config = require('./config/config.json');
 
+
+// Router File =================================
+const routes = require('./routes/routes.js');
+app.use('/', routes);
+
+// Start ya engines! ===========================
 app.listen(config.port, function() {
     logger.warn('test warning');
     logger.error('test error');
